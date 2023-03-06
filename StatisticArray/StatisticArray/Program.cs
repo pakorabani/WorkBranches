0429634c9e182788916c13f2905ad8f4a3755385
Пако Рабани <124561897+pakorabani@users.noreply.github.com>
2023-03-06 11:23:02 +0200

    Update Program.cs

--- StatisticArray/StatisticArray/Program.cs
+++ StatisticArray/StatisticArray/Program.cs
@@ -6,6 +6,7 @@ namespace StatisticArray
     {
         static void Main(string[] args)
         {
+            //haist kotara4eta
             int n = int.Parse(Console.ReadLine());
             int[] arr = new int[n];
             for (int i = 0; i < n; i++)
